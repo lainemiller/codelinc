dfb28e48ef25f45c9b29dcc72c90d1434ac39c65
lincolncodelinc <79852648+lincolncodelinc@users.noreply.github.com>
2021-03-01 10:23:06 -0500

    added sample files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # codelinc
+
+This project contains source code and supporting files for a serverless application that you can deploy with the AWS codePipeline  It includes the following files and folders.
+
+- hello-world - Code for the application's Lambda function.
+- events - Invocation events that you can use to invoke the function.
+- hello-world/tests - Unit tests for the application code.
+- template.yaml - A template that defines the application's AWS resources.
+
+## Deploy the sample application
+
+To build and deploy your application for the first time, run the following in your shell:
---
+++ hello-world/app.js
@@ -0,0 +1,101 @@
+
+"use strict";
+const AWS = require('aws-sdk');
+
+const db = new AWS.DynamoDB.DocumentClient();
+let response;
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+exports.lambdaHandler = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Greetings My world',
+
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
+exports.addName = async (event, context) => {
+    let response;
+
+    const id= new Date().getTime()+'x';
+
+
+   const name  = JSON.parse(event.body); // fetch name property from POST request body
+   console.log('------Name='+name );
+
+
+  console.log('------id='+id );
+
+    const params = {
+        TableName: 'Names',
+        Item: {
+            id,
+            name
+        }
+    };
+
+    try {
+        await db.put(params).promise();
+        response = {
+            statusCode: 201,
+        };
+    } catch (err) {
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({ message: err.message })
+        };
+    }
+
+    response = {
+            statusCode: 201,
+        };
+
+    return response;
+}
+exports.greetNames = async (event, context) => {
+    let response;
+
+console.log('-------entere dgreetNames function');
+    try {
+        const names = (await db.scan({ TableName: 'Names' }).promise())
+            .Items.map((item) => item.name);
+console.log('-------names map----' ,JSON.stringify(names));
+
+const data=JSON.stringify(names);
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+              message: `hello Me changed`+data
+            })
+        };
+    } catch (err) {
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err.message,
+            })
+        }
+    }
+
+    return response;
+}
