3d452eff81dbba9df4ed63d3d1dd252cdd671166
Siddanagowda C K <Siddanagowda.K@mindtree.com>
2022-09-27 20:15:25 +0530

    latest changes made to ia form

--- src/initialAssessmentFormsHandler/iaFormP2.js
+++ src/initialAssessmentFormsHandler/iaFormP2.js
@@ -34,34 +34,19 @@ EmpAndEdu = (edu) => {
         if (error) {
           return reject(error);
         }
+
         return resolve(results);
       }
     );
   });
 };
 
-socialHistory = (social) => {
-  return new Promise((resolve, reject) => {
-    pool.query(
-      QUERIES.InitialAssessment.page2SocialPost, social,
-      (error, results) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(results);
-      }
-    );
-  });
-};
-
-module.exports = async function (edu, mental, social) {
+module.exports = async function (edu, mental) {
   try {
     const pg1MH = await MentalHealth(mental);
     const pg1EAE = await EmpAndEdu(edu);
-    const pg1Soc = await socialHistory(social);
     console.log('leg', pg1MH);
     console.log(pg1EAE);
-    console.log(pg1Soc);
   } catch (error) {
     console.log(error);
   }
