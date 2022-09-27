e0ae25e26a61a5bb9637765d26fa8cbd6ef17a67
Siddanagowda C K <Siddanagowda.K@mindtree.com>
2022-09-27 20:20:05 +0530

    uploadImage

--- src/imageUploadService/uploadImage.js
+++ src/imageUploadService/uploadImage.js
@@ -1,49 +1,47 @@
 const aws = require('aws-sdk');
-   const multer = require('multer');
-   const multerS3 = require('multer-s3');
-   const https = require('https');
-   const fs = require('fs');
-   //const dotenv = require('dotenv');
-   //dotenv.config();
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const https = require('https');
+const fs = require('fs');
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-  
-   aws.config.update({
-    secretAccessKey: "6v/hXrJj3YWllnobXYJxyNA7UVIy5P5MGp+KvAl3",
-    accessKeyId:  "AKIASX4D4UGUWXQMLUUC",
-    region: 'us-east-1' //E.g us-east-1
-   });
+aws.config.update({
+  secretAccessKey: '6v/hXrJj3YWllnobXYJxyNA7UVIy5P5MGp+KvAl3',
+  accessKeyId: 'AKIASX4D4UGUWXQMLUUC',
+  region: 'us-east-1' // E.g us-east-1
+});
 
-   const s3 = new aws.S3();
+const s3 = new aws.S3();
 
-   /* In case you want to validate your file type */
-   const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-     cb(null, true);
-    } else {
-     cb(new Error('Wrong file type, only upload JPEG and/or PNG !'), 
-     false);
-    }
-   };
+/* In case you want to validate your file type */
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Wrong file type, only upload JPEG and/or PNG !'),
+      false);
+  }
+};
 
-   const upload = multer({
-   fileFilter: fileFilter,
-   storage: multerS3({
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
     acl: 'public-read',
     s3,
     bucket: 'servant-center-profileph-bucket',
     metadata: function (req, file, cb) {
-        cb(null, {fieldName: file.fieldname});
-      },
-    key: function(req, file, cb) {
-      /*I'm using Date.now() to make sure my file has a unique name*/
-      req.file = Date.now() +file.originalname;
-      cb(null, Date.now() +file.originalname);
-     }
-    })
-   });
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      /* I'm using Date.now() to make sure my file has a unique name */
+      req.file = Date.now() + file.originalname;
+      cb(null, Date.now() + file.originalname);
+    }
+  })
+});
 
-   module.exports = upload;
-
+module.exports = upload;
 
 // const fs = require('fs')
 // const S3 = require('aws-sdk/clients/s3')
@@ -72,4 +70,3 @@ const aws = require('aws-sdk');
 //   return s3.upload(uploadParams).promise()
 // }
 // exports.uploadFile = uploadFile
-
