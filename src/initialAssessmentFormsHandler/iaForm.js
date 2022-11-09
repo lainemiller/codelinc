30a26e8172018119d7118ef88d1c01dddbf551cb
Akash kumar <akash.kumar6@mindtree.com>
2022-11-09 12:45:11 +0530

    Db changes

--- src/initialAssessmentFormsHandler/iaForm.js
+++ src/initialAssessmentFormsHandler/iaForm.js
@@ -1,13 +1,30 @@
-const secrets = require('../secret');
-
 const { Pool } = require('pg');
 const { QUERIES } = require('../constants');
+const AWS = require('aws-sdk');
+const region = 'us-east-1';
+var dbCredential = {};
+
+const client = new AWS.SecretsManager({
+  region
+});
+
+client.getSecretValue({ SecretId: 'dev/postgres/codelinc/db' }, function (err, data) {
+  if (err) {
+    if (err.code === 'DecryptionFailureException') { throw err; } else if (err.code === 'InternalServiceErrorException') { throw err; } else if (err.code === 'InvalidParameterException') { throw err; } else if (err.code === 'InvalidRequestException') { throw err; } else if (err.code === 'ResourceNotFoundException') { throw err; }
+  } else {
+    if ('SecretString' in data) {
+      let secret = data.SecretString;
+      dbCredential = JSON.parse(secret);
+    }
+  }
+});
+
 const pool = new Pool({
-  host: secrets.HOST,
-  user: secrets.USER,
-  password: secrets.DBENTRY,
-  database: secrets.DATABASE,
-  port: secrets.PORT
+  host: dbCredential.host,
+  user: dbCredential.username,
+  password: dbCredential.password,
+  database: dbCredential.dbname,
+  port: dbCredential.port
 });
 
 const LeaglHist = (legal) => {
--- src/secret.js
+++ src/secret.js
@@ -1,9 +1,4 @@
 module.exports = Object.freeze({
   REGION: 'us-east-1',
-  HOST: 'codelinc-dev-instance-1.cc3ixdgzorsu.us-east-1.rds.amazonaws.com',
-  USER: 'codelinc_api',
-  DBENTRY: 'sjr930Qhy',
-  DATABASE: 'codelinc',
-  PORT: '5432',
   BUCKET: 'servant-center-profileph-bucket'
 });
