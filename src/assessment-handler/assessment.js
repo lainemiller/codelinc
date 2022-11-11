6e7a91b96a7a74e50c133223d6c2034d00a81566
Yuvaganesh17 <Yuvaganesh.S@mindtree.com>
2022-11-11 21:36:57 +0530

    dbcredentials

--- src/assessment-handler/assessment.js
+++ src/assessment-handler/assessment.js
@@ -9,24 +9,35 @@ const client = new AWS.SecretsManager({
   region
 });
 
-client.getSecretValue({ SecretId: 'dev/postgres/codelinc/db' }, function (err, data) {
-  if (err) {
-    if (err.code === 'DecryptionFailureException') { throw err; } else if (err.code === 'InternalServiceErrorException') { throw err; } else if (err.code === 'InvalidParameterException') { throw err; } else if (err.code === 'InvalidRequestException') { throw err; } else if (err.code === 'ResourceNotFoundException') { throw err; }
-  } else {
-    if ('SecretString' in data) {
-      let secret = data.SecretString;
-      dbCredential = JSON.parse(secret);
-    }
-  }
-});
+getCredential();
 
-const pool = new Pool({
-  host: dbCredential.host,
-  user: dbCredential.username,
-  password: dbCredential.password,
-  database: dbCredential.dbname,
-  port: dbCredential.port
-});
+function getCredential() {
+    client.getSecretValue({ SecretId: 'dev/postgres/codelinc/db' }, function (err, data) {
+    if (err) {
+      console.log("ERROR")
+      console.log(err)
+      if (err.code === 'DecryptionFailureException') { throw err; } else if (err.code === 'InternalServiceErrorException') { throw err; } else if (err.code === 'InvalidParameterException') { throw err; } else if (err.code === 'InvalidRequestException') { throw err; } else if (err.code === 'ResourceNotFoundException') { throw err; }
+    } else {
+      console.log("SUCCESS")
+      if ('SecretString' in data) {
+        let secret = data.SecretString;
+        dbCredential = JSON.parse(secret);
+        dbConnection();
+      }
+    }
+  });
+}
+
+let pool;
+function dbConnection() {
+  pool = new Pool({
+    host: dbCredential.host,
+    user: dbCredential.username,
+    password: dbCredential.password,
+    database: dbCredential.dbname,
+    port: dbCredential.port
+  });
+}
 
 const PIQuery = (vet) => {
   return new Promise((resolve, reject) => {
