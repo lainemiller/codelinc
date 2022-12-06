f004c8ab56c4ee3fdf2f9801f367cc3652145215
Sam Kamalesan <sam.kamalesan@lfg.com>
2022-12-06 22:44:05 +0530

    adding consoles

--- src/treatmentPlan-handler/updateTreatment.js
+++ src/treatmentPlan-handler/updateTreatment.js
@@ -30,7 +30,7 @@ function getCredential () {
           throw err;
         }
       } else {
-        console.log('SUCCESS');
+        console.log('SUCCESS updateTreatment');
         if ('SecretString' in data) {
           const secret = data.SecretString;
           dbCredential = JSON.parse(secret);
