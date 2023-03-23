d1a2ce41df8336002babaa7d7caa41127e87dc71
Yuvaganesh17 <Yuvaganesh.S@mindtree.com>
2023-03-23 19:44:55 +0530

    fetching uploaded files

--- src/miscFileUploadService/uploadFile.js
+++ src/miscFileUploadService/uploadFile.js
@@ -1,8 +1,8 @@
-const aws = require('aws-sdk');
-const secret = require('../secret');
+const aws = require("aws-sdk");
+const secret = require("../secret");
 
 const s3 = new aws.S3({
-    region:secret.REGION
+  region: secret.REGION,
 });
 
 const uploadToS3 = (imageBody, fileName) => {
@@ -10,9 +10,9 @@ const uploadToS3 = (imageBody, fileName) => {
     const uploadParams = {
       Bucket: "servant-center-miscfile-bucket",
       Key: fileName,
-      Body: imageBody
+      Body: imageBody,
     };
-    console.log('files resp', uploadParams);
+    console.log("files resp", uploadParams);
 
     s3.upload(uploadParams, (err, data) => {
       if (err) {
@@ -25,4 +25,23 @@ const uploadToS3 = (imageBody, fileName) => {
   });
 };
 
-module.exports = { uploadToS3 };
+const getUserFilesFromS3 = (prefix) => {
+  return new Promise((resolve, reject) => {
+    const getParams = {
+      Bucket: "servant-center-miscfile-bucket",
+      Delimiter: "/",
+      Prefix: prefix + "/",
+    };
+    console.log(getParams);
+    s3.listObjectsV2(getParams, (err, data) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      console.log(data);
+      resolve(data);
+    });
+  });
+};
+
+module.exports = { uploadToS3, getUserFilesFromS3 };
