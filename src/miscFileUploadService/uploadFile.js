4ce8e9059dffc12deb5318af4635152772689e94
Sam Kamalesan <sam.kamalesan@lfg.com>
2023-04-01 20:44:12 +0530

    upload file with mime type

--- src/miscFileUploadService/uploadFile.js
+++ src/miscFileUploadService/uploadFile.js
@@ -1,16 +1,17 @@
-const aws = require("aws-sdk");
-const secret = require("../secret");
+const aws = require('aws-sdk');
+const secret = require('../secret');
 
 const s3 = new aws.S3({
-  region: secret.REGION,
+  region: secret.REGION
 });
 
-const uploadToS3 = (imageBody, fileName) => {
+const uploadToS3 = (imgFile, fileName) => {
   return new Promise((resolve, reject) => {
     const uploadParams = {
-      Bucket: "servant-center-miscfile-bucket",
+      Bucket: 'servant-center-miscfile-bucket',
       Key: fileName,
-      Body: imageBody,
+      Body: imgFile?.buffer,
+      'Content-Type': imgFile?.mimetype
     };
 
     s3.upload(uploadParams, (err, data) => {
@@ -25,9 +26,9 @@ const uploadToS3 = (imageBody, fileName) => {
 const getUserFilesFromS3 = (prefix) => {
   return new Promise((resolve, reject) => {
     const getParams = {
-      Bucket: "servant-center-miscfile-bucket",
-      Delimiter: "/",
-      Prefix: prefix + "/",
+      Bucket: 'servant-center-miscfile-bucket',
+      Delimiter: '/',
+      Prefix: prefix + '/'
     };
     s3.listObjectsV2(getParams, (err, data) => {
       if (err) {
@@ -41,8 +42,8 @@ const getUserFilesFromS3 = (prefix) => {
 const downloadFilesFromS3 = (key) => {
   return new Promise((resolve, reject) => {
     const getParams = {
-      Bucket: "servant-center-miscfile-bucket",
-      Key: key,
+      Bucket: 'servant-center-miscfile-bucket',
+      Key: key
     };
     s3.getObject(getParams, (err, data) => {
       if (err) {
