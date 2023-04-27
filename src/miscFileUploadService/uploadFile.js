40ea023de0c5cc5064ede0a5f643e08e8b4bf4f4
Yuvaganesh17 <Yuvaganesh.S@mindtree.com>
2023-04-27 15:16:19 +0530

    new commit

--- src/miscFileUploadService/uploadFile.js
+++ src/miscFileUploadService/uploadFile.js
@@ -1,48 +1,19 @@
 const aws = require('aws-sdk');
-const secrets = require('../secret');
 const region = 'us-east-1';
-let fileCredential = {};
-
-const client = new aws.SecretsManager({
-  region
-});
-
-client.getSecretValue({ SecretId: 'photo/s3' }, function (err, data) {
-  if (err) {
-    if (err.code === 'DecryptionFailureException') {
-      throw err;
-    } else if (err.code === 'InternalServiceErrorException') {
-      throw err;
-    } else if (err.code === 'InvalidParameterException') {
-      throw err;
-    } else if (err.code === 'InvalidRequestException') {
-      throw err;
-    } else if (err.code === 'ResourceNotFoundException') {
-      throw err;
-    }
-  } else {
-    if ('SecretString' in data) {
-      const secret = data.SecretString;
-      fileCredential = JSON.parse(secret);
-    }
-  }
-});
 
 const s3 = new aws.S3({
-  secretAccessKey: fileCredential.secretKey,
-  accessKeyId: fileCredential.accessKey,
-  region: secrets.REGION
+  region
 });
 
 const uploadToS3 = (imgFile, fileName) => {
   return new Promise((resolve, reject) => {
     const uploadParams = {
-      Bucket: "servant-center-profileph-bucket",
+      Bucket: "servant-center-miscfile-bucket",
       Key: fileName,
       Body: imgFile.buffer,
       ContentType: imgFile.mimetype,
     };
-    console.log(uploadParams);
+    console.log("upload params",uploadParams);
     s3.putObject(uploadParams, (err, data) => {
       if (err) {
         console.log("misc files uploadToS3:", err);
@@ -83,19 +54,21 @@ const downloadFilesFromS3 = (key) => {
         console.log('misc files downloadFilesFromS3 pdf:', err);
         reject(err);
       }
-      if (data.ContentType.indexOf('pdf') > 0) {
-        console.log('misc files downloadFilesFromS3 pdf:', data);
-        resolve(data);
-      } else {
-        s3.getSignedUrl('getObject', getParams, (err, urlStr) => {
-          if (err) {
-            console.log('misc files downloadFilesFromS3 image:', err);
-            reject(err);
-          }
-          console.log('misc files downloadFilesFromS3 image:', urlStr);
-          resolve(urlStr);
-        });
-      }
+      console.log("file download response from s3",data);
+      resolve(data);
+      // if (data.ContentType.indexOf('pdf') > 0) {
+      //   console.log('misc files downloadFilesFromS3 pdf:', data);
+      //   resolve(data);
+      // } else {
+      //   s3.getSignedUrl('getObject', getParams, (err, urlStr) => {
+      //     if (err) {
+      //       console.log('misc files downloadFilesFromS3 image:', err);
+      //       reject(err);
+      //     }
+      //     console.log('misc files downloadFilesFromS3 image:', urlStr);
+      //     resolve(urlStr);
+      //   });
+      // }
     });
   });
 };
